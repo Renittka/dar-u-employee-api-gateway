df734f20d6c6fb9c9bba7e66a29a162295659ff4
Anna <renita5701@gmail.com>
2023-02-11 12:07:53 +0600

    Changed application properties for gateway

---
+++ src/test/java/kz/dar/university/employeeapigateway/EmployeeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dar.university.employeeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
